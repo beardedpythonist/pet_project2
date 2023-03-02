f15534950a4f2c9cdd02c7ea021004d800c56bf9
Alexander Gordey <alexnickgordey3@gmail.com>
2023-03-02 15:29:52 +0300

    шаблон пришит на 20%

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [path('', index11, name='home'),
-               path('about/', about, name='about'),
+               path('about.html/', about, name='about'),
                path('single-post.html', addpage, name='add_page'),
                path('contact/', contact, name='contact'),
                path('login/', login, name='login'),
