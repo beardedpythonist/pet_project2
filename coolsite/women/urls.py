aefabd40217c241da3e2a87b71e1778a6c89a716
Alexander Gordey <alexnickgordey3@gmail.com>
2023-03-09 12:51:57 +0300

    новый шаблон. все красиво! не раббботают только ссылки на небазовом сайте

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -3,10 +3,10 @@ from .views import *
 
 
 urlpatterns = [path('', WomenHome.as_view(), name='home'),
+               path('index.html', WomenHome.as_view(), name='home'),
                path('register.html', RegisterUSer.as_view(), name='register'),
                path('login/', login, name='login'),
                path('addpage.html', add_page, name='add_page'),
-               path('contact/', contact, name='contact'),
                path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
                path('search-result.html', search, name='search'),
                path('cats/<slug:cat_slug>/', CategoryListView.as_view(), name='category'),
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -8,17 +8,11 @@ from .utils import *
 from .forms import *
 from .models import *
 
-menu = [{'title': 'О сайте', 'url_name': 'about'},
-        {'title': 'добавить статью', 'url_name': 'add_page'},
-        {'title': 'обратная связь', 'url_name': 'contact'},
-        {'title': 'войти', 'url_name': 'login'}]
-
 
 class WomenHome(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'post'
-
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -26,57 +20,37 @@ class WomenHome(DataMixin, ListView):
         context['last_sport'] = Women.objects.filter(cat__pk=2).order_by("-id")[0:5]
         context['last_sing'] = Women.objects.filter(cat__pk=3).order_by("-id")[0:5]
         context['last_mode'] = Women.objects.filter(cat__pk=3).order_by("-id")[0:5]
-
-
-
+        context['last_all'] = Women.objects.filter().order_by("-id")[0:5]
+        context['cats'] = Category.objects.all()
         return context
 
 
 class CategoryListView(DataMixin, ListView):
-
     template_name = 'women/category.html'
-    context_object_name = 'cats'
+    context_object_name = 'posts'
+
 
     def get_queryset(self):
-        print(Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True))
         return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
-
-
-
-    # def get_context_data(self, *, object_list=None, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['cats'] = Category.objects.all()
-    #     print(context)
-    #     return context
-
 
 class ShowPost(DataMixin, DetailView):
     model = Women
-    template_name = 'women/single-post.html'
+    template_name = 'women/single.html'
     slug_url_kwarg = 'post_slug'
     context_object_name = 'post'
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['cats'] = Category.objects.all()
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     return Women.objects.filter(slug__slug=self.kwargs['post_slug'])
+    #
 
-        return context
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+    raise_exception = True
 
 
-def pageNotFound(request, exception):
-    return HttpResponseNotFound(' <h1> страница не найдена  </h1>')
-
-# class AddPage(LoginRequiredMixin, DataMixin, CreateView):
-#     form_class = AddPostForm
-#     template_name = 'women/addpage.html'
-#     success_url = reverse_lazy('home')
-#     login_url = reverse_lazy('home')
-#     raise_exception = True
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         c_def = self.get_user_context(title="Добавление статьи")
-#         return dict(list(context.items()) + list(c_def.items()))
 
 
 def contact(request):
